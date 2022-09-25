c6b60e13e0d5e2c93288d29c6b8d473b7d9083c3
bird8696 <bird8696@naver.com>
2022-09-25 17:30:56 +0900

    ddd

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,26 @@ import AppIndex from "./AppIndex";
 export const SetContext = React.createContext({});
 
 function App() {
+  const navigation = useNavigate();
+  const [dispatchType, setDispatechType] = React.useState({
+    code: "",
+    params: null,
+  });
+  const [viichan, setViichan] = React.useState([
+    {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0,
+        E: 0,
+        S: 0,
+    }
+  ]);
+
+  React.useEffect(() => {
+    switch(dispatchType.code)
+  })
+
   return (
     <SetContext.Provider>
       <AppIndex />
