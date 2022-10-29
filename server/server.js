0593745c5f810c3e067af642394d96882eb8b8fe
bird8696 <bird8696@naver.com>
2022-10-29 16:23:40 +0900

    aaa

--- server/server.js
+++ server/server.js
@@ -111,12 +111,37 @@ app.post("./join", (req, res) => {
 
 app.post("/Viichan", function (req, res) {
   const Viichan = req.query.Viichan;
+  //   const Viichan2 = Viichan.map((item) => {
+  //     return JSON.parse(item);
+  //   });
+
   let ViichanStr = "";
+
+  //   Viichan2.forEach((item) => {
+  //     const keys = Object.keys(item);
+  //     const values = Object.values(item);
+  //     const maxStr = values[0] >= values[1] ? keys[0] : keys[1];
+  //     ViichanStr += maxStr;
+  //   });
+  DB.viichan = Viichan;
+
   res.send(ViichanStr);
 });
 
 app.get("/result", function (req, res) {
   console.log(DB);
+  console.log(DB.viichan.S);
+
+  let max = 0;
+  let maxkey = "";
+
+  for (const test in DB.viichan) {
+    const value = DB.viichan[test];
+
+    max = Math.max(max, value);
+  }
+  console.log(max);
+  res.send("");
 });
 
 app.listen(5000, function () {
